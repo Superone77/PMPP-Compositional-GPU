979bd7a13d5d8b0f2aa5a99f93056e636f22c56a
One7 <yangwq177@163.com>
2022-01-08 00:02:27 +0100

    timer4

--- source/main.cpp
+++ source/main.cpp
@@ -361,28 +361,58 @@ void timer3(PatIntPtr <std::pair<std::vector < int>, std::vector<int>>, int> s_p
 		<< " sec" << std::endl;
 }
 
+void timer4(PatIntPtr<std::vector<int>, std::vector<int>> s_ptr) {
+    auto create = [](int size) {
+        auto vec = std::vector<int>(size);
+        std::generate(vec.begin(), vec.end(), rand);
+        return vec;
+    };
+
+    auto wrap = [](std::vector<int> vec) {
+        std::promise <std::vector<int>> prom;
+        prom.set_value(std::move(vec));
+        return prom.get_future();
+    };
+
+    std::cout << "Testing Time of : " << s_ptr->Name() << std::endl;
+    std::cout << "repeats: " << repeats << std::endl;
+    std::cout << "size: " << size << std::endl;
+
+
+
+    std::vector < std::future < std::vector < int>>> inputs;
+    std::vector <std::future<std::vector < int>>> outputs;
+
+    for (auto i = 0; i < repeats; i++) {
+        auto vec = create(size);
+        auto wrapped = std::move(wrap(std::move(vec)));
+        inputs.emplace_back(std::move(wrapped));
+    }
+	auto start = system_clock::now();
+    s_ptr->Init();
+
+
+    for (auto i = 0; i < repeats; i++) {
+        outputs.emplace_back(std::move(s_ptr->Compute(std::move(inputs[i]))));
+    }
+
+
+    for (auto i = 0; i < repeats; i++) {
+        auto &output = outputs[i];
+        auto val = output.get();
+    }
+
+    s_ptr->Dispose();
+	auto end = system_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    std::cout << "average time of " << s_ptr->Name() << " is "
+              << (double(duration.count()) * microseconds::period::num / microseconds::period::den) / repeats
+              << " sec" << std::endl;
+}
 
 int main(int argument_count, char **arguments) {
     {
-//		auto max = std::make_shared<DotPro_GPU<int>>();
-//		auto max_w = AlgorithmWrapper<<std::vector<int>,std::vector<int>>, int>::create(max);
-//
-//		auto qs = std::make_shared<QuickSorter<int>>();
-//		auto qs_w = AlgorithmWrapper<std::vector<int>, std::vector<int>>::create(qs);
-//
-//		const auto num_threads_1 = 1;
-//		const auto num_threads_2 = 7;
-//
-//		// const auto num_threads_1 = 7;
-//		// const auto num_threads_2 = template&OOP;
-//
-//		auto tp = TaskPool<std::vector<int>, std::vector<int>>::create(qs_w, num_threads_1);
-//		auto tp2 = TaskPool<std::vector<int>, int>::create(max_w, num_threads_2);
-//
-//		auto comp = Composition<std::vector<int>, std::vector<int>, int>::create(tp, tp2);
-//
-////		test2(comp);
-
+        const auto num_threads_1 = 1;
 //GPU
         auto nop = std::make_shared < Nop_GPU < int >> ();
         auto nop_w = AlgorithmWrapper<int, int>::create(nop);
@@ -392,18 +422,19 @@ int main(int argument_count, char **arguments) {
         auto min_w = AlgorithmWrapper < std::vector < int >,int > ::create(min);
         auto dot_pro = std::make_shared < DotPro_GPU < int >> ();
         auto dot_pro_w = AlgorithmWrapper < std::pair < std::vector < int >, std::vector<int>>, int > ::create(dot_pro);
-        const auto num_threads_1 = 1;
+
         auto tp_nop = TaskPool<int, int>::create(nop_w, num_threads_1);
         auto tp_max = TaskPool < std::vector < int >,int > ::create(max_w, num_threads_1);
         auto tp_min = TaskPool < std::vector < int >,int > ::create(min_w, num_threads_1);
-        auto tp_dot_pro = TaskPool < std::pair < std::vector < int >, std::vector<int>>, int > ::create(dot_pro_w,
-                                                                                                        num_threads_1);
+        auto tp_dot_pro = TaskPool < std::pair < std::vector < int >, std::vector<int>>, int > ::create(dot_pro_w,num_threads_1);
 //CPU
 //TODO
+        const auto num_threads_2 = 8;
 		auto qs = std::make_shared<QuickSorter<int>>();
 		auto qs_w = AlgorithmWrapper<std::vector<int>, std::vector<int>>::create(qs);
 		auto inc = std::make_shared<Increaser<int>>();
 		auto inc_w = AlgorithmWrapper<std::vector<int>, std::vector<int>>::create(inc);
+        auto tp_qs = TaskPool<std::vector<int>, std::vector<int>>::create(qs_w,num_threads_2);
 
 
 //test
@@ -416,7 +447,8 @@ int main(int argument_count, char **arguments) {
 //		timer1(tp_nop);
 //      timer2(tp_min);
 //      timer2(tp_max);
-	  timer3(tp_dot_pro);
+	  //timer3(tp_dot_pro);
+      timer4(tp_qs);
 
 //timer for composition
 //TODO
