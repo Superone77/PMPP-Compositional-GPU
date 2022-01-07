55fbcdcadc0fd69e691ff501555774e61621ce5b
One7 <yangwq177@163.com>
2022-01-07 13:04:54 +0100

    max/min test

--- source/main.cpp
+++ source/main.cpp
@@ -20,12 +20,11 @@
 #include "pattern/ForkJoin.hpp"
 #include "helper/Comparator.hpp"
 
-void test1(PatIntPtr<std::vector<int>, std::vector<int>> s_ptr)
+void testMax(PatIntPtr<std::vector<int>, int> s_ptr)
 {
 	auto create = [](int size) {
 		auto vec = std::vector<int>(size);
 		std::generate(vec.begin(), vec.end(), rand);
-		for(int i = 0; i< size;i++) printf("%d", vec[i]);
 		return vec;
 	};
 
@@ -37,61 +36,71 @@ void test1(PatIntPtr<std::vector<int>, std::vector<int>> s_ptr)
 
 	std::cout << "Testing: " << s_ptr->Name() << std::endl;
 
-	const auto repeats = 1000;
-	const auto size = 65536;
-
-	std::vector<std::future<std::vector<int>>> inputs;
-	std::vector<std::future<std::vector<int>>> outputs;
-
-	for (auto i = 0; i < repeats; i++)
-	{
-		auto vec = create(size);
-		auto wrapped = std::move(wrap(std::move(vec)));
-		inputs.emplace_back(std::move(wrapped));
-	}
-
-	s_ptr->Init();
-
-	for (auto i = 0; i < repeats; i++)
-	{
-		outputs.emplace_back(std::move(s_ptr->Compute(std::move(inputs[i]))));
-	}
-
-	for (auto i = 0; i < repeats; i++)
-	{
-		auto &output = outputs[i];
-		auto val = output.get();
-		// printf("%d",std::is_sorted(val.begin(), val.end()));
-	}
-
-	s_ptr->Dispose();
-}
-void test2(PatIntPtr<std::vector<int>, int> s_ptr)
-{
-	auto create = [](int size) {
-		auto vec = std::vector<int>(size);
-		std::generate(vec.begin(), vec.end(), rand);
-//		for(int i = 0; i< size;i++) printf("%d ", vec[i]);
-		return vec;
-	};
-
-	auto wrap = [](std::vector<int> vec) {
-		std::promise<std::vector<int>> prom;
-		prom.set_value(std::move(vec));
-		return prom.get_future();
-	};
-
-	std::cout << "Testing: " << s_ptr->Name() << std::endl;
-
-	const auto repeats = 1;
+	const auto repeats = 100;
 	const auto size = 10;
 
 	std::vector<std::future<std::vector<int>>> inputs;
 	std::vector<std::future<int>> outputs;
+    std::vector<int> validation;
 
 	for (auto i = 0; i < repeats; i++)
 	{
 		auto vec = create(size);
+		auto maxValue = *max_element(vec.begin(),vec.end());
+        validation.emplace_back(maxValue);
+        auto wrapped = std::move(wrap(std::move(vec)));
+		inputs.emplace_back(std::move(wrapped));
+	}
+
+	s_ptr->Init();
+
+	for (auto i = 0; i < repeats; i++)
+	{
+		outputs.emplace_back(std::move(s_ptr->Compute(std::move(inputs[i]))));
+	}
+
+	for (auto i = 0; i < repeats; i++)
+	{
+		auto &output = outputs[i];
+		auto val = output.get();
+        if(val != validation[i]){
+            std::cout<<"Find Max element false"<<std::endl;
+            return;
+        }
+	}
+
+	s_ptr->Dispose();
+	std::cout<<"Test finding Max successfully"<<std::endl;
+}
+
+void testMin(PatIntPtr<std::vector<int>, int> s_ptr)
+{
+	auto create = [](int size) {
+		auto vec = std::vector<int>(size);
+		std::generate(vec.begin(), vec.end(), rand);
+		return vec;
+	};
+
+	auto wrap = [](std::vector<int> vec) {
+		std::promise<std::vector<int>> prom;
+		prom.set_value(std::move(vec));
+		return prom.get_future();
+	};
+
+	std::cout << "Testing: " << s_ptr->Name() << std::endl;
+
+	const auto repeats = 100;
+	const auto size = 10;
+
+	std::vector<std::future<std::vector<int>>> inputs;
+	std::vector<std::future<int>> outputs;
+	std::vector<int> validation;
+
+	for (auto i = 0; i < repeats; i++)
+	{
+		auto vec = create(size);
+		auto maxValue = *min_element(vec.begin(),vec.end());
+		validation.emplace_back(maxValue);
 		auto wrapped = std::move(wrap(std::move(vec)));
 		inputs.emplace_back(std::move(wrapped));
 	}
@@ -100,10 +109,6 @@ void test2(PatIntPtr<std::vector<int>, int> s_ptr)
 
 	for (auto i = 0; i < repeats; i++)
 	{
-//		auto input = inputs[i].get();
-//		for(int j = 0; j<size;j++){
-//			printf("%d",input[j]);
-//		}
 		outputs.emplace_back(std::move(s_ptr->Compute(std::move(inputs[i]))));
 	}
 
@@ -111,34 +116,49 @@ void test2(PatIntPtr<std::vector<int>, int> s_ptr)
 	{
 		auto &output = outputs[i];
 		auto val = output.get();
-//		printf("%d",val);
+		if(val != validation[i]){
+			std::cout<<"Find Min element false"<<std::endl;
+			return;
+		}
 	}
 
 	s_ptr->Dispose();
+	std::cout<<"Test finding Min successfully"<<std::endl;
+
 }
-void test3(PatIntPtr<std::vector<int>, int> s_ptr)
+
 
 int main(int argument_count, char **arguments)
 {
 	{
-		auto max = std::make_shared<DotPro_GPU<int>>();
-		auto max_w = AlgorithmWrapper<<std::vector<int>,std::vector<int>>, int>::create(max);
+//		auto max = std::make_shared<DotPro_GPU<int>>();
+//		auto max_w = AlgorithmWrapper<<std::vector<int>,std::vector<int>>, int>::create(max);
+//
+//		auto qs = std::make_shared<QuickSorter<int>>();
+//		auto qs_w = AlgorithmWrapper<std::vector<int>, std::vector<int>>::create(qs);
+//
+//		const auto num_threads_1 = 1;
+//		const auto num_threads_2 = 7;
+//
+//		// const auto num_threads_1 = 7;
+//		// const auto num_threads_2 = template&OOP;
+//
+//		auto tp = TaskPool<std::vector<int>, std::vector<int>>::create(qs_w, num_threads_1);
+//		auto tp2 = TaskPool<std::vector<int>, int>::create(max_w, num_threads_2);
+//
+//		auto comp = Composition<std::vector<int>, std::vector<int>, int>::create(tp, tp2);
+//
+////		test2(comp);
+        auto max = std::make_shared<Max_GPU<int>>();
+		auto max_w = AlgorithmWrapper<std::vector<int>, int>::create(max);
+		auto min = std::make_shared<Min_GPU<int>>();
+		auto min_w = AlgorithmWrapper<std::vector<int>, int>::create(min);
+        const auto num_threads_1 = 2;
+        auto tp_max = TaskPool<std::vector<int>, int>::create(max_w, num_threads_1);
+		auto tp_min = TaskPool<std::vector<int>, int>::create(min_w, num_threads_1);
+        testMax(tp_max);
+		testMin(tp_min);
 
-		auto qs = std::make_shared<QuickSorter<int>>();
-		auto qs_w = AlgorithmWrapper<std::vector<int>, std::vector<int>>::create(qs);
-
-		const auto num_threads_1 = 1;
-		const auto num_threads_2 = 7;
-
-		// const auto num_threads_1 = 7;
-		// const auto num_threads_2 = template&OOP;
-
-		auto tp = TaskPool<std::vector<int>, std::vector<int>>::create(qs_w, num_threads_1);
-		auto tp2 = TaskPool<std::vector<int>, int>::create(max_w, num_threads_2);
-
-		auto comp = Composition<std::vector<int>, std::vector<int>, int>::create(tp, tp2);
-
-//		test2(comp);
 	}
 	std::cout << "Finished" << std::endl;
 
